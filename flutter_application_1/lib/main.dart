cd05005b2ee47732bd4b38d1496d417b31626d24
ridho6 <muhammad.ridho.syahputra18@gmail.com>
2025-06-14 11:05:16 +0800

    Initial commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Muhammad Ridho Syahputra',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Text('Halo Selamat Datang Di Flutter'),
+      ),
+      ),
+    );
+  }
+}
